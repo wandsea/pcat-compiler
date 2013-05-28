4b07e19e59ae6d4555af3874b24d48c83c0961dc
Wandsea <wandsea@gmail.com>
2013-05-28 11:36:34 +0800

    init linux branch

--- src/compile.sh
+++ src/compile.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-
-gcc -g -m32 -c sup.c
+if [ -f "./parser" ]; then
+./parser $1
+else
+make all
+./parser $1
+fi
+rm data.s code.s
+gcc -g -m32 -c sup.s
 gcc -g -m32 -c pcat.s
-gcc -g -m32 sup.o pcat.o -o exe
+gcc -g -m32 sup.o pcat.o -o test.o
+#rm pcat.s
+#make clean
+
--- src/data.s
+++
@@ -1,8 +0,0 @@
-
-	 .section .rdata,"dr"
-label_1:
-	 .ascii "C = \0"
-
-	 .section .rdata,"dr"
-label_2:
-	 .ascii " (should be 4)\0"
