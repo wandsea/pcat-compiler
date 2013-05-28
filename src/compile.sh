93b989887c22ae74691c09a7420f8a7df1a88f8f
Wandsea <wandsea@gmail.com>
2013-05-28 12:00:02 +0800

    work with gcc4.8 on linux

--- src/compile.sh
+++ src/compile.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 if [ -f "./parser" ]; then
-./parser $1
+./parser $1 > $1.log
 else
 make all
-./parser $1
+./parser $1 > $1.log
 fi
 rm data.s code.s
-gcc -g -m32 -c sup.s
+gcc -g -m32 -c sup.c
 gcc -g -m32 -c pcat.s
-gcc -g -m32 sup.o pcat.o -o test.o
+gcc -g -m32 sup.o pcat.o -o $1.o
 #rm pcat.s
 #make clean
 
